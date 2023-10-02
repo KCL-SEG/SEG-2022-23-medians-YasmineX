2c4240deab3a5056004fe75bb39f71770aef1c33
Yasmin Alzaid <1xyasminex@gmail.com>
2023-10-02 12:44:19 +0100

    Completed Exercise

--- median.py
+++ median.py
@@ -1,6 +1,15 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(lst):
+    lst.sort()
+    if len(lst) % 2 == 0:
+        med = (lst[len(lst)//2] + lst[len(lst)//2 - 1]) / 2
+    else:
+        med = lst[len(lst)//2]
+    return med
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +18,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median(numbers))
